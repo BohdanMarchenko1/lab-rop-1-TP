078e2ff9b536daab010fe541c4289a93a61dc725
Богдан Марченко <bogdanmarchenko90@gmail.com>
2024-03-06 14:28:35 +0200

    add project

---
+++ Form1.Designer.cs
@@ -0,0 +1,331 @@
+﻿namespace Краші_гірші_непорівняня_альтернатив_V2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.labe1 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.labe2 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.labe3 = new System.Windows.Forms.Label();
+            this.button8 = new System.Windows.Forms.Button();
+            this.la1 = new System.Windows.Forms.Label();
+            this.la2 = new System.Windows.Forms.Label();
+            this.la3 = new System.Windows.Forms.Label();
+            this.la4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label19 = new System.Windows.Forms.Label();
+            this.labelBest = new System.Windows.Forms.Label();
+            this.labelWorst = new System.Windows.Forms.Label();
+            this.la31 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(36, 145);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 0;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(36, 248);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 0;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(36, 39);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 13);
+            this.label8.TabIndex = 4;
+            // 
+            // labe1
+            // 
+            this.labe1.AutoSize = true;
+            this.labe1.Location = new System.Drawing.Point(37, 65);
+            this.labe1.Name = "labe1";
+            this.labe1.Size = new System.Drawing.Size(40, 13);
+            this.labe1.TabIndex = 5;
+            this.labe1.Text = "ввели:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(36, 129);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(33, 13);
+            this.label10.TabIndex = 4;
+            this.label10.Text = "макс";
+            // 
+            // labe2
+            // 
+            this.labe2.AutoSize = true;
+            this.labe2.Location = new System.Drawing.Point(36, 172);
+            this.labe2.Name = "labe2";
+            this.labe2.Size = new System.Drawing.Size(37, 13);
+            this.labe2.TabIndex = 6;
+            this.labe2.Text = "ввели";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(39, 229);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(27, 13);
+            this.label12.TabIndex = 7;
+            this.label12.Text = "наш";
+            // 
+            // labe3
+            // 
+            this.labe3.AutoSize = true;
+            this.labe3.Location = new System.Drawing.Point(36, 275);
+            this.labe3.Name = "labe3";
+            this.labe3.Size = new System.Drawing.Size(37, 13);
+            this.labe3.TabIndex = 8;
+            this.labe3.Text = "ввели";
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(300, 104);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 2;
+            this.button8.Text = "button6";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // la1
+            // 
+            this.la1.AutoSize = true;
+            this.la1.Location = new System.Drawing.Point(297, 145);
+            this.la1.Name = "la1";
+            this.la1.Size = new System.Drawing.Size(33, 13);
+            this.la1.TabIndex = 10;
+            this.la1.Text = "гірші:";
+            // 
+            // la2
+            // 
+            this.la2.AutoSize = true;
+            this.la2.Location = new System.Drawing.Point(300, 186);
+            this.la2.Name = "la2";
+            this.la2.Size = new System.Drawing.Size(35, 13);
+            this.la2.TabIndex = 11;
+            this.la2.Text = "краші";
+            // 
+            // la3
+            // 
+            this.la3.AutoSize = true;
+            this.la3.Location = new System.Drawing.Point(297, 229);
+            this.la3.Name = "la3";
+            this.la3.Size = new System.Drawing.Size(42, 13);
+            this.la3.TabIndex = 12;
+            this.la3.Text = "всього";
+            // 
+            // la4
+            // 
+            this.la4.AutoSize = true;
+            this.la4.Location = new System.Drawing.Point(297, 290);
+            this.la4.Name = "la4";
+            this.la4.Size = new System.Drawing.Size(65, 13);
+            this.la4.TabIndex = 13;
+            this.la4.Text = "непорівняні";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(36, 42);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 0;
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Location = new System.Drawing.Point(36, 26);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(101, 13);
+            this.label19.TabIndex = 9;
+            this.label19.Text = "кількість критеріїв";
+            // 
+            // labelBest
+            // 
+            this.labelBest.AutoSize = true;
+            this.labelBest.Location = new System.Drawing.Point(33, 402);
+            this.labelBest.Name = "labelBest";
+            this.labelBest.Size = new System.Drawing.Size(50, 13);
+            this.labelBest.TabIndex = 14;
+            this.labelBest.Text = "labelBest";
+            // 
+            // labelWorst
+            // 
+            this.labelWorst.AutoSize = true;
+            this.labelWorst.Location = new System.Drawing.Point(33, 445);
+            this.labelWorst.Name = "labelWorst";
+            this.labelWorst.Size = new System.Drawing.Size(57, 13);
+            this.labelWorst.TabIndex = 15;
+            this.labelWorst.Text = "labelWorst";
+            // 
+            // la31
+            // 
+            this.la31.AutoSize = true;
+            this.la31.Location = new System.Drawing.Point(300, 255);
+            this.la31.Name = "la31";
+            this.la31.Size = new System.Drawing.Size(106, 13);
+            this.la31.TabIndex = 12;
+            this.la31.Text = "всього гіпотетично:";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1185, 556);
+            this.tabControl1.TabIndex = 16;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.la31);
+            this.tabPage1.Controls.Add(this.labelWorst);
+            this.tabPage1.Controls.Add(this.textBox4);
+            this.tabPage1.Controls.Add(this.labelBest);
+            this.tabPage1.Controls.Add(this.textBox5);
+            this.tabPage1.Controls.Add(this.la4);
+            this.tabPage1.Controls.Add(this.textBox6);
+            this.tabPage1.Controls.Add(this.button8);
+            this.tabPage1.Controls.Add(this.la3);
+            this.tabPage1.Controls.Add(this.label8);
+            this.tabPage1.Controls.Add(this.la2);
+            this.tabPage1.Controls.Add(this.label10);
+            this.tabPage1.Controls.Add(this.la1);
+            this.tabPage1.Controls.Add(this.labe1);
+            this.tabPage1.Controls.Add(this.label19);
+            this.tabPage1.Controls.Add(this.labe2);
+            this.tabPage1.Controls.Add(this.labe3);
+            this.tabPage1.Controls.Add(this.label12);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1177, 530);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridView1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1177, 530);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 6);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1165, 518);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1209, 580);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "кількість критеріїв";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lab1;
+        private System.Windows.Forms.Label lab2;
+        private System.Windows.Forms.Label lab3;
+        private System.Windows.Forms.Label lab4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label labe1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label labe2;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label labe3;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label la1;
+        private System.Windows.Forms.Label la2;
+        private System.Windows.Forms.Label la3;
+        private System.Windows.Forms.Label la4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Label labelBest;
+        private System.Windows.Forms.Label labelWorst;
+        private System.Windows.Forms.Label la31;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Краші_гірші_непорівняня_альтернатив_V2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        static DataTable FPrit(List<object[]> matr)
+        {
+
+            DataTable table = new DataTable();
+
+
+
+            // Добавление столбцов в DataTable
+            for (int i = 0; i < matr[0].Length; i++)
+            {
+                table.Columns.Add($"Column{i}");
+            }
+
+            // Добавление данных в DataTable
+            for (int i = 0; i < matr.Count; i++)
+            {
+                DataRow row = table.NewRow();
+                for (int j = 0; j < matr[0].Length; j++)
+                {
+                    row[j] = "k"+(j+1)+matr[i][j];
+                }
+                table.Rows.Add(row);
+            }
+
+
+            return table;
+        }
+
+
+
+        private void button8_Click(object sender, EventArgs e)//розрахунок
+
+        {
+            int NMasCret;
+            int[] maxMasCret;
+            int[] MyMasCret;
+
+
+            //заповнення 1:
+            NMasCret=9;
+            maxMasCret = Enumerable.Repeat(3, NMasCret).ToArray();
+
+            //оберемо альтернативу: 1,2,3, 3,2,1, 2,2,1.
+            //заповнення 2:
+
+            MyMasCret =new int[NMasCret];
+            MyMasCret[0]=1;
+            MyMasCret[1]=2;
+            MyMasCret[2]=3;
+
+            MyMasCret[3]=3;
+            MyMasCret[4]=2;
+            MyMasCret[5]=1;
+
+            MyMasCret[6]=2;
+            MyMasCret[7]=2;
+            MyMasCret[8]=1;
+
+
+            //заповненя 1:
+
+            //NMasCret = 3;
+            //maxMasCret= new int[NMasCret];
+            //maxMasCret[0]=3;
+            //maxMasCret[1]=3;
+            //maxMasCret[2]=7;
+            //заповненя 2:
+
+            //MyMasCret=new int[NMasCret];
+            //MyMasCret[0]=2;
+            //MyMasCret[1]=2;
+            //MyMasCret[2]=3;
+
+
+
+
+
+            NMasCret= int.Parse(textBox4.Text);
+            maxMasCret= new int[NMasCret];
+            MyMasCret=new int[NMasCret];
+
+            ////заповненя 1
+            labe2.Text="ввели: ";
+            string[] words = textBox5.Text.Split(' ');
+
+            for (int i = 0; i<maxMasCret.Length; i++)
+            {
+                maxMasCret[i]=int.Parse(words[i]);
+                labe2.Text += maxMasCret[i];
+            }
+
+            ////заповненя 2
+            labe3.Text="ввели: ";
+            string[] words1 = textBox6.Text.Split(' ');
+
+            for (int i = 0; i<MyMasCret.Length; i++)
+            {
+                MyMasCret[i]=int.Parse(words1[i]);
+                labe2.Text += maxMasCret[i];
+            }
+
+
+            //
+            labe1.Text="кількість критеріїв: "+NMasCret;
+            labelBest.Text="Краща: (";
+            labelWorst.Text="Гірша: (";
+            for (int i = 0; i<maxMasCret.Length; i++)
+            {
+                labelBest.Text+="k"+(i+1)+1+", ";
+                labelWorst.Text+="k"+(i+1)+maxMasCret[i]+", ";
+            }
+            //видалення останніх двох символів
+            labelBest.Text=labelBest.Text.Remove(labelBest.Text.Length - 2);
+            labelWorst.Text=labelWorst.Text.Remove(labelWorst.Text.Length - 2);
+
+            labelBest.Text+=")";
+            labelWorst.Text+=")";
+
+
+            int bad = 1;
+            for (int i = 0; i < maxMasCret.Length; ++i)
+            {
+                bad*= MyMasCret[i];
+            }
+            bad -= 1;
+            la1.Text="краші: "+bad;
+
+            int good = 1;
+            for (int i = 0; i < maxMasCret.Length; ++i)
+            {
+                good*=maxMasCret[i]-MyMasCret[i]+1;
+            }
+            good-=1;
+            la2.Text="гірші: "+good;
+
+
+            int all = 1;
+            for (int i = 0; i < maxMasCret.Length; ++i)
+            {
+                all *=maxMasCret[i];
+            }
+
+            la31.Text="всього гіпотетично: "+all;
+
+            //хитро:
+            int difrent = all-good-bad-1;
+            la4.Text="непорівняні: "+difrent;
+
+            la3.Text="всього фактично: "+(difrent+good+bad+1);
+
+
+
+
+
+        }
+    }
+}
