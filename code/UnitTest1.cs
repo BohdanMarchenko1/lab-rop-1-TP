3ae8d41df108d4d0e8a8c2ef37164db1ed130088
Богдан Марченко <bogdanmarchenko90@gmail.com>
2024-03-14 13:49:47 +0200

    complete refactoring

--- code/UnitTest1.cs
+++ code/UnitTest1.cs
@@ -8,54 +8,55 @@ namespace UnitTestProject1
     [TestClass]
     public class UnitTest1
     {
-        int[] MaxMasCret = { 3, 3, 7 };
-        int[] MyMasCret = { 2, 2, 3 };
-        Form1 f = new Form1();
+        static int NMasCret = 3;
+        static int[] MaxMasCret = { 3, 3, 7 };
+        static int[] MyMasCret = { 2, 2, 3 };
+        Calculation MyCalc = new Calculation(NMasCret, MaxMasCret, MyMasCret);
 
         //тест конвертації
-        [TestMethod]
+       [TestMethod]
         public void TestMetod_StrArrIntoIntArr()
         {
-
-            string[] words = { "3", "3", "7" };
-            CollectionAssert.AreEqual(MaxMasCret, f.StrArrIntoIntArr(words));
+            StrInputData f=new StrInputData("3", "3 3 7", "2 2 3");
+            CollectionAssert.AreEqual(MaxMasCret, f.StrArrIntoIntArr(f.StrMaxMasCret));
         }
         //тести виводу
         [TestMethod]
+
         public void TestMethod_StringBest()
         {
-            Assert.AreEqual("Краща: (k11, k21, k31)", f.StringBest(3));
+            Assert.AreEqual("Краща: (k11, k21, k31)", MyCalc.StringBest());
         }
         [TestMethod]
         public void TestMetod_StringWorst()
         {
-            Assert.AreEqual("Гірша: (k13, k23, k37)", f.StringWorst(MaxMasCret));
+            Assert.AreEqual("Гірша: (k13, k23, k37)", MyCalc.StringWorst());
         }
         //тести обчислення
-        //[TestMethod]
-        //public void TestMetod_CountBad()
-        //{
-            
-        //    Assert.AreEqual(19, f.CountBad(MyMasCret, MaxMasCret));
-        //}
-        //[TestMethod]
-        //public void TestMetod_CountGood()
-        //{
-            
-        //    Assert.AreEqual(11, f.CountGood(MyMasCret));
-        //}
-        //[TestMethod]
-        //public void TestMetod_CountAll()
-        //{
-            
-        //    Assert.AreEqual(63, f.CountAll(MaxMasCret));
-        //}
-        //[TestMethod]
-        //public void TestMetod_CountDifrent()
-        //{
-            
-        //    Assert.AreEqual(32, f.CountDifrent(MyMasCret,MaxMasCret));
-        //}
+        [TestMethod]
+        public void TestMetod_CountBad()
+        {
+
+            Assert.AreEqual(19, MyCalc.CountBad());
+        }
+        [TestMethod]
+        public void TestMetod_CountGood()
+        {
+
+            Assert.AreEqual(11, MyCalc.CountGood());
+        }
+        [TestMethod]
+        public void TestMetod_CountAll()
+        {
+
+            Assert.AreEqual(63, MyCalc.CountAll());
+        }
+        [TestMethod]
+        public void TestMetod_CountDifrent()
+        {
+
+            Assert.AreEqual(32, MyCalc.CountDifrent());
+        }
 
     }
 }
